75de3b19050178bf9978f2b64717b364b766fbf6
PhanHieu244 <phanhieu2443@gmail.com>
2025-04-27 00:47:29 +0700

    no iap

---
+++ Assets/Candypuzzleblock/Scripts/AdmobTest.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+
+public class AdmobTest : MonoBehaviour
+{
+
+    public void Start()
+    {
+    }
+
+
+
+
+    public void ShowInterstitialAd()
+
+    {
+        print("show inter");
+
+        if (Advertisements.Instance.IsInterstitialAvailable())
+
+        {
+            Advertisements.Instance.ShowInterstitial();
+            print("show inter");
+
+        }
+    }
+
+ 
+
+    public void ShowRewardedVideo()
+    {
+#if UNITY_EDITOR
+ StackManager.Instance.ContinueGameplay();
+        GamePlay.Instance.InstantRescue();
+        print("show rewarded video");
+
+#endif
+        if (Advertisements.Instance.IsRewardVideoAvailable())
+        {
+            Advertisements.Instance.ShowRewardedVideo(CompleteMethod);
+        }else
+
+        {
+            Advertisements.Instance.ShowInterstitial();
+
+        }
+    }
+   
+
+    private void CompleteMethod(bool completed, string advertiser)
+    {
+        if (completed == true)
+        {
+            //give the reward
+            StackManager.Instance.ContinueGameplay();
+            GamePlay.Instance.InstantRescue();
+        }
+else
+            {
+                //no reward
+        }
+            }
+
+
+
+  
+
+}
